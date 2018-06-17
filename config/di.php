c377524bb1c627a4560a9dd6f829a33393c59a4f
Taras <audi@audiophilesoft.ru>
2018-06-17 16:30:05 +0300

    added DI in one line for Settings

--- config/di.php
+++ config/di.php
@@ -4,6 +4,5 @@ return [
     \App\Writer\WriterInterface::class => DI\get(\App\Writer\Text::class),
     \App\Output\OutputInterface::class => DI\get(\App\Output\Console::class),
     \App\Profiler\ProfilerInterface::class => DI\get(\App\Profiler\SimpleProfiler::class),
-    \App\Settings::class => DI\autowire()
-        ->constructor(__DIR__, 'app.php')
+    \App\Settings::class => DI\autowire()->constructorParameter('dir', __DIR__)->constructorParameter('file_name', 'app.php')
 ];
